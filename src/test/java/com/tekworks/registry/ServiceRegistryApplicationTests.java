7fa435f16cb17fec1624f644a8664789025c813e
Yash-123patel <patelyash250702@gmail.com>
2025-04-03 15:56:51 +0530

    Service registry eureka server

---
+++ src/test/java/com/tekworks/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekworks.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
